aa0518afcd7f5770645b3e7ccd750dfa115ebe5a
kyoungsik jeung <kyoungsikjeung@kyoungsikui-MacBookPro.local>
2025-03-30 03:54:14 +0900

    추가

---
+++ BinarySearchTree03/main.cpp
@@ -0,0 +1,21 @@
+#include "BST.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <int> data = {10,30,20,40,50,60,70};
+    BST bst;
+    for(int i = 0; i < data.size(); i++){
+        bst.addLeaf(data[i]);
+    }
+    
+    cout << "In-order traversal: ";
+    bst.printInOrder();
+    bst.returnNode(20);
+    bst.returnNode(100);
+
+    bst.printChild(10);
+
+    return 0;
+}
